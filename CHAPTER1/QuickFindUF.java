4f12c23b4ad14c5e20ae978d978e59645ce16171
wangbangkun <wangbangkun@yeah.net>
2014-03-27 23:40:11 +0800

    20140327

---
+++ CHAPTER1/QuickFindUF.java
@@ -0,0 +1,48 @@
+/*
+ * Develop classes QuickFindUF that implement quick-find.
+ */
+public class QuickFindUF
+{
+    private int [] id;
+    private int count;
+
+    public QuickFindUF(int N)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            this.id[i] = i;
+        }
+        this.count = N;
+    }
+
+    public void union(int p, int q)
+    {
+        if (this.connected(p, q))
+        {
+            return;
+        }
+        for (int i = 0; i < this.id.length; i++)
+        {
+            if (this.id[i] == this.find(p))
+            {
+                this.id[i] = this.find(q);
+            }
+        }
+        this.count--;
+    }
+
+    public int find(int p)
+    {
+        return this.id[p];
+    }
+
+    public boolean connected(int p, int q)
+    {
+        return this.id[p] == this.id[q];
+    }
+
+    public int count()
+    {
+        return this.count;
+    }
+}
